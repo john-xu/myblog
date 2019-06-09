9486770687c3ed2377e4049ffa3727312d9d131c
john-xu <bigappleofxxc@gmail.com>
2019-06-09 21:24:35 +0800

    add

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '慕远久航',
+    description: '慕远的小码头',
+    themeConfig: {
+        nav: [
+            { text: 'nodejs', link: '/nodejs/'}
+        ],
+        // displayAllHeaders: true,
+        sidebar: {
+            '/nodejs/': [
+                ['', 'nodejs'],
+                ['pm2', 'pm2使用']
+            ]
+        }
+    }
+}
